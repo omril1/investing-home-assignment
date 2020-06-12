fc89f5ad913a3cd8bd97f2b326c81df6d8c5cc8e
omril1 <omri2593@gmail.com>
2020-06-12 20:06:46 +0300

    Add filter that doesn't work, let's do some TDD to fix it

--- front-end/src/routes/instruments/InstrumentsList.tsx
+++ front-end/src/routes/instruments/InstrumentsList.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 import * as style from './style.scss';
 import Instrument from './Instrument';
 import instrumentsAPI, { Instrument as InstrumentApi } from './instrumentsAPI';
-import { action, observable } from 'mobx';
+import { action, observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 
 @observer
 export default class InstrumentsList extends React.Component {
   @observable private instruments: InstrumentApi[] = [];
+  @observable private searchText = '';
+
   @action private setInstruments = (instruments: InstrumentApi[]) => (this.instruments = instruments);
+  @action private setSearch = (e: React.ChangeEvent<HTMLInputElement>) => (this.searchText = e.target.value);
+
+  @computed private get filteredInstruments() {
+    return this.instruments;
+  }
 
   componentDidMount() {
     instrumentsAPI.getAllInstruments().then(this.setInstruments);
@@ -17,8 +24,18 @@ export default class InstrumentsList extends React.Component {
   render() {
     return (
       <div>
+        <input
+          className={style.searchBox}
+          type="text"
+          placeholder="Search"
+          name="search"
+          id="search"
+          value={this.searchText}
+          onChange={this.setSearch}
+        />
+
         <ul className={style.InstrumentsList}>
-          {this.instruments.map(instrument => (
+          {this.filteredInstruments.map(instrument => (
             <Instrument key={instrument.instrumentId} instrument={instrument} />
           ))}
         </ul>
