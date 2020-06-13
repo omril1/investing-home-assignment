c076a06e151a56be8c8dac193b09e7943fcc4ba3
omril1 <omri2593@gmail.com>
2020-06-13 12:17:25 +0300

    Add some styling

--- front-end/src/routes/instruments/Instrument.tsx
+++ front-end/src/routes/instruments/Instrument.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import * as style from './style.scss';
 import { Instrument as InstrumentApi } from './instrumentsAPI';
 
 const Instrument: React.FC<{ instrument: InstrumentApi }> = props => {
   const content = JSON.stringify(props.instrument, null, 2).split('"');
 
   return (
-    <li>
+    <li className={style.instrument}>
       <pre>
         {content.map((c, i) => (
           <span key={i} style={{ color: `hsl(${Math.random() * 360}, 60%, 40%)` }}>
